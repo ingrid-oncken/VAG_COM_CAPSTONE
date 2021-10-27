c1dd60c1fd56907d37036ce69fd5ce9f56ff0c7a
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-10-27 21:55:47 +0100

    navbar skeleton

--- vag-com-proj/src/App.js
+++ vag-com-proj/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg'
+import MainNavBar from './Components/MainNavBar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
   return (
-    <div className="App App-header">
+    <div>
       
-      
+      <MainNavBar />
     </div>
   )
 }
---
+++ vag-com-proj/src/Components/MainNavBar.jsx
@@ -0,0 +1,50 @@
+import { Component } from 'react'
+import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+
+class MainNavBar extends Component {
+  render() {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              //  src="../Component/media/VCDS_logo.bmp"
+              src="http://www.vag-com-portugues.com/VCDS_logo.bmp"
+              //  width="30"
+              height="80"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Funções</Nav.Link>
+              <Nav.Link href="#link">Simulador Virtual</Nav.Link>
+              <Nav.Link href="#link">Registro</Nav.Link>
+              <Nav.Link href="#link">Downloads</Nav.Link>
+              <Nav.Link href="#link">Interfaces</Nav.Link>
+              <Nav.Link href="#link">Falsificações</Nav.Link>
+              <NavDropdown title="FAQ" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.2">
+                  Perguntas frequentes
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Português" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.2">Español</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Italiano</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">English</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Deutsch</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#link"><AiOutlineShoppingCart /></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  }
+}
+
+export default MainNavBar
