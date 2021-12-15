fb2485ea0620150a52e34b954e83590e90bdd198
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-12-15 11:46:46 +0000

    ok

--- vag-com-proj/src/Components/MainCarousel.jsx
+++ vag-com-proj/src/Components/MainCarousel.jsx
@@ -5,83 +5,114 @@ class MainCarousel extends Component {
   render() {
     return (
       <>
-        <Carousel>
-          <Carousel.Item>
-            <Row>
-              <Col xs={6} md={4}>
-                <Image
-                  className="d-block w-100"
-                  src="../img/placeholders/370x420_prod_1.jpg"
-                  rounded
-                />
-              </Col>
-              <Col xs={6} md={4}>
-                <Image
-                  src="../img/placeholders/370x420_prod_2.jpg"
-                  rounded
-                  className="d-block w-100"
-                />
-              </Col>
-              <Col xs={6} md={4}>
-                <Image
-                  src="../img/placeholders/370x420_prod_3.jpg"
-                  rounded
-                  className="d-block w-100"
-                />
-              </Col>
-            </Row>
-          </Carousel.Item>
-          <Carousel.Item>
-            <Row>
-              <Col xs={6} md={4}>
-                <Image
-                  src="../img/placeholders/370x420_prod_4.jpg"
-                  rounded
-                  className="d-block w-100"
-                />
-              </Col>
-              <Col xs={6} md={4}>
-                <Image
-                  src="../img/placeholders/370x420_prod_5.jpg"
-                  rounded
-                  className="d-block w-100"
-                />
-              </Col>
-              <Col xs={6} md={4}>
-                <Image
-                  src="../img/placeholders/370x420_prod_6.jpg"
-                  rounded
-                  className="d-block w-100"
-                />
-              </Col>
-            </Row>{' '}
-          </Carousel.Item>
-          <Carousel.Item>
-            <Row>
-              <Col xs={6} md={4}>
-                <Image
-                  src="../img/placeholders/370x420_prod_7.jpg"
-                  rounded
-                  className="d-block w-100"
-                />
-              </Col>
-              <Col xs={6} md={4}>
-                <Image
-                  src="../img/placeholders/370x420_prod_8.jpg"
-                  rounded
-                  className="d-block w-100"
-                />
-              </Col>
-              <Col xs={6} md={4}>
-                <Image
-                  src="../img/placeholders/370x420_prod_9.jpg"
-                  rounded
-                  className="d-block w-100"
-                />
-              </Col>
-            </Row>
-          </Carousel.Item>
-        </Carousel>
+        {/* TRIPPLE IMAGE CAROUSEL */}
+        {/* className="d-none d-sm-none d-md-none d-lg-block" */}
+        <Row className="justify-content-center d-none d-sm-none d-md-none d-lg-block">
+          <Carousel>
+            <Carousel.Item>
+              <Row>
+                <Col xs={6} md={4}>
+                  <Image
+                    className="d-block w-100"
+                    src="../img/placeholders/370x420_prod_1.jpg"
+                    rounded
+                  />
+                </Col>
+                <Col xs={6} md={4}>
+                  <Image
+                    src="../img/placeholders/370x420_prod_2.jpg"
+                    rounded
+                    className="d-block w-100"
+                  />
+                </Col>
+                <Col xs={6} md={4}>
+                  <Image
+                    src="../img/placeholders/370x420_prod_3.jpg"
+                    rounded
+                    className="d-block w-100"
+                  />
+                </Col>
+              </Row>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Row>
+                <Col xs={6} md={4}>
+                  <Image
+                    src="../img/placeholders/370x420_prod_4.jpg"
+                    rounded
+                    className="d-block w-100"
+                  />
+                </Col>
+                <Col xs={6} md={4}>
+                  <Image
+                    src="../img/placeholders/370x420_prod_5.jpg"
+                    rounded
+                    className="d-block w-100"
+                  />
+                </Col>
+                <Col xs={6} md={4}>
+                  <Image
+                    src="../img/placeholders/370x420_prod_6.jpg"
+                    rounded
+                    className="d-block w-100"
+                  />
+                </Col>
+              </Row>{' '}
+            </Carousel.Item>
+            <Carousel.Item>
+              <Row>
+                <Col xs={6} md={4}>
+                  <Image
+                    src="../img/placeholders/370x420_prod_7.jpg"
+                    rounded
+                    className="d-block w-100"
+                  />
+                </Col>
+                <Col xs={6} md={4}>
+                  <Image
+                    src="../img/placeholders/370x420_prod_8.jpg"
+                    rounded
+                    className="d-block w-100"
+                  />
+                </Col>
+                <Col xs={6} md={4}>
+                  <Image
+                    src="../img/placeholders/370x420_prod_9.jpg"
+                    rounded
+                    className="d-block w-100"
+                  />
+                </Col>
+              </Row>
+            </Carousel.Item>
+          </Carousel>
+        </Row>
+        {/* className="d-lg-none d-xl-none" */}
+        {/* SINGLE IMAGE CAROUSEL */}
+        <Row className="justify-content-center d-lg-none d-xl-none">
+          <Carousel fade>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="../img/placeholders/370x420_prod_1.jpg"
+                //alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="../img/placeholders/370x420_prod_2.jpg"
+                //alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="../img/placeholders/370x420_prod_3.jpg"
+                //alt="Third slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </Row>
       </>
     )
   }
